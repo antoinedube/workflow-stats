744e136e55e9cd5f66fada94b641ba7b82359467
Antoine Dubé <antoine.dube@gmail.com>
2016-07-13 15:55:41 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+app.use(morgan('combined'));
+
+app.use('/', express.static('client/templates'));
+app.use('/bower_components', express.static('bower_components'));
+app.use('/javascripts', express.static('client/javascripts'));
+
+// Do stuff for different model/api thingy.
+// ['api-token', 'issue', 'transition'].forEach(function (name) { require(name).register(); });
+
+app.listen(3000);
