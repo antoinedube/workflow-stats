278bc2ce615f5a2b22cb3b9c21e4e6d6f1c6ea64
Antoine Dubé <antoine.dube@gmail.com>
2016-08-16 15:44:55 -0400

    Added recipes.

---
+++ chef/workflowstats/recipes/generate_api_key.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: workflowstats
+# Recipe:: generate_api_key
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
---
+++ chef/workflowstats/recipes/seed_data.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: workflowstats
+# Recipe:: seed_data
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
