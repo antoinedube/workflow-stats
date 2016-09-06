517b35dd263f4ff5363acd54602f63b32c66afc1
Antoine Dubé <antoine.dube@gmail.com>
2016-09-06 13:30:58 -0400

    Added AWS PG settings.

--- chef/workflowstats/recipes/postgres_prod.rb
+++ chef/workflowstats/recipes/postgres_prod.rb
@@ -4,16 +4,17 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+postgres_settings = search(:aws_opsworks_rds_db_instance).first
 template '/home/vagrant/workflow-stats/server/database/pg-config.json' do
   source 'pg-config.json.erb'
   owner 'vagrant'
   group 'vagrant'
   variables(
-    :database => "wfs",
-    :username => "wfs",
-    :password => "wfs",
-    :host => "localhost",
-    :port => "5432",
-    :dialect => "postgres"
+    :database => 'wfs',
+    :username => postgres_settings['db_user'],
+    :password => postgres_settings['db_password'],
+    :hostname => postgres_settings['address'],
+    :port => postgres_settings['port']
+    :dialent => 'postgres'
   )
 end
