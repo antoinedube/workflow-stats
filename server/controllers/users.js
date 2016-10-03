e298f913d7a0f0aea8321c7f0b0293d099b1a36b
Antoine Dubé <antoine.dube@gmail.com>
2016-10-03 15:09:46 -0400

    Syntax update

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,15 +1,15 @@
 var models = require('../models/index');
-var local_strategy = require('../passport/local-strategy.js');
+var apiTokenStrategy = require('../passport/api-token-strategy.js');
+var localStrategy = require('../passport/local-strategy.js');
 
 var register = function(app) {
-
-  app.get('/users', local_strategy.isLoggedIn, function(req, res) {
+  app.get('/users', localStrategy.isLoggedIn, function(req, res) {
     models.user.findAll({}).then(function(users) {
       res.json({ data: users });
     });
   });
 
-  app.get('/users/:id', local_strategy.isLoggedIn, function(req, res) {
+  app.get('/users/:id', localStrategy.isLoggedIn, function(req, res) {
     models.user.find({
       where: { id: req.params.id }
     }).then(function(user) {
@@ -17,11 +17,12 @@ var register = function(app) {
     });
   });
 
-  app.post('/users', local_strategy.isLoggedIn, function(req, res) {
+  app.post('/users', apiTokenStrategy.isTokenValid, function(req, res) {
     var username = req.body.username;
+    var email = req.body.email;
     var password = req.body.password;
 
-    var user = models.user.build({ username: username, email: 'abc@company.com' });
+    var user = models.user.build({ username: username, email: email });
     user.setPassword(password);
     user.save().then(function(savedUser) {
       res.json({ data: savedUser });
