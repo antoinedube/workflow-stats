9ab774ca7f1a8e0ca06c4da39194790690ab42dc
Antoine Dubé <antoine.dube@gmail.com>
2016-10-05 16:02:56 -0400

    Attempt to fix seed

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,33 +2,37 @@ var models = require('../models/index');
 var apiTokenStrategy = require('../passport/api-token-strategy.js');
 var localStrategy = require('../passport/local-strategy.js');
 
-var register = function(app) {
-  app.get('/users', localStrategy.isLoggedIn, function(req, res) {
-    models.user.findAll({}).then(function(users) {
-      res.json({ data: users });
-    });
+function index(req, res) {
+  models.user.findAll({}).then(function(users) {
+    return res.json({ data: users });
   });
+}
 
-  app.get('/users/:id', localStrategy.isLoggedIn, function(req, res) {
-    models.user.find({
-      where: { id: req.params.id }
-    }).then(function(user) {
-      res.json({ data: user });
-    });
+function view(req, res) {
+  models.user.find({
+    where: { id: req.params.id }
+  }).then(function(user) {
+    return res.json({ data: user });
   });
+}
 
-  app.post('/users', apiTokenStrategy.isTokenValid, function(req, res) {
-    var username = req.body.username;
-    var email = req.body.email;
-    var password = req.body.password;
+function create(req, res) {
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
 
-    var user = models.user.build({ username: username, email: email });
-    user.setPassword(password);
-    user.save().then(function(savedUser) {
-      res.json({ data: savedUser });
-    });
+  var user = models.user.build({ username: username, email: email });
+  user.setPassword(password);
+  user.save().then(function(savedUser) {
+    return res.json({ data: savedUser });
   });
-};
+}
+
+function register(app) {
+  app.get('/users', localStrategy.isLoggedIn, index);
+  app.get('/users/:id', localStrategy.isLoggedIn, view);
+  app.post('/users', apiTokenStrategy.isTokenValid, create);
+}
 
 module.exports = {
   register: register
